030ff219997fb1e77a1a4f51651a84b7a72a2e9c
Daniel Augustin <daniel.augustin1@gmx.de>
2020-08-31 23:48:38 +0200

    Implement ClassDiagramIterator

--- src/main/java/class_diagram_editor/diagram/ClassDiagram.java
+++ src/main/java/class_diagram_editor/diagram/ClassDiagram.java
@@ -1,10 +1,25 @@
 package class_diagram_editor.diagram;
 
 import class_diagram_editor.code_generation.CodeElement;
+import lombok.Getter;
 
-import java.util.Iterator;
+import java.util.*;
 
 public class ClassDiagram {
+
+    private final Map<String, CodeElement> codeElements;
+
+    public ClassDiagram() {
+        this.codeElements = new HashMap<>();
+    }
+
+    public void addClass(Class c) {
+        codeElements.put(c.getName(), c);
+    }
+
+    public Collection<CodeElement> getCodeElements() {
+        return new ArrayList<>(codeElements.values());
+    }
 
     public Iterator<CodeElement> iterator() {
         return new ClassDiagramIterator(this);
@@ -12,20 +27,26 @@ public class ClassDiagram {
 
     private static class ClassDiagramIterator implements Iterator<CodeElement> {
 
-        private final ClassDiagram classDiagram;
+        private final List<CodeElement> codeElements;
+        private int currentElement;
 
         public ClassDiagramIterator(ClassDiagram classDiagram) {
-            this.classDiagram = classDiagram;
+            this.currentElement = 0;
+            this.codeElements = new ArrayList<>(classDiagram.getCodeElements());
         }
 
         @Override
         public boolean hasNext() {
-            return false;
+            return currentElement < codeElements.size();
         }
 
         @Override
         public CodeElement next() {
-            return null;
+            int temp = currentElement;
+
+            currentElement++;
+
+            return codeElements.get(temp);
         }
     }
 
